43d31f18441f76f07fce4b9e3876f7583d2da96e
myoung <myoung@catalyte.io>
2022-03-28 16:20:05 -0400

    add six filters

--- src/Catalyte.Apparel.Data/Filters/ProductFilters.cs
+++ src/Catalyte.Apparel.Data/Filters/ProductFilters.cs
@@ -13,5 +13,34 @@ namespace Catalyte.Apparel.Data.Filters
         {
             return products.Where(p => p.Id == productId).AsQueryable();
         }
+
+        public static IQueryable<Product> WhereProductBrandEquals(this IQueryable<Product> products, string brand)
+        {
+            return products.Where(p => p.Brand == brand).AsQueryable();
+        }
+
+        public static IQueryable<Product> WhereProductCategoryEquals(this IQueryable<Product> products, string category)
+        {
+            return products.Where(p => p.Category == category).AsQueryable();
+        }
+
+        public static IQueryable<Product> WhereProductDemographicEquals(this IQueryable<Product> products, string demographic)
+        {
+            return products.Where(p => p.Demographic == demographic).AsQueryable();
+        }
+
+        public static IQueryable<Product> WhereProductPriceEquals(this IQueryable<Product> products, string price)
+        {
+            return products.Where(p => p.Price == price).AsQueryable();
+        }
+        public static IQueryable<Product> WhereProductColorEquals(this IQueryable<Product> products, string color)
+        {
+            return products.Where(p => p.Color == color).AsQueryable();
+        }
+        public static IQueryable<Product> WhereProductMaterialEquals(this IQueryable<Product> products, string material)
+        {
+            return products.Where(p => p.Material == material).AsQueryable();
+        }
+
     }
 }
