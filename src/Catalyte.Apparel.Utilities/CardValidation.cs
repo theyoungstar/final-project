37adbc875d2d0137ce05c3cdc27820044d0ec088
Julius Amaker <jamaker@catalyte.io>
2022-03-28 12:38:10 -0700

    Created CC Validation file and add some logic

--- src/Catalyte.Apparel.Utilities/CardValidation.cs
+++ src/Catalyte.Apparel.Utilities/CardValidation.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace Catalyte.Apparel.Utilites
@@ -14,96 +15,130 @@ namespace Catalyte.Apparel.Utilites
     
     public class CardValidation
     {
-        private static void DateFormat(string field)
+        /*public static bool IsCreditCardInfoValid(string cardNo, string expiryDate, string cvv)
         {
-            Regex regex = new Regex(@"/^(0[1-9]|1[0-2])\/?([0-9]{2})$/", RegexOptions.Compiled);
+            var cardCheck = new Regex(@"^(1298|1267|4512|4567|8901|8933)([\-\s]?[0-9]{4}){3}$");
+            var monthCheck = new Regex(@"^(0[1-9]|1[0-2])$");
+            var yearCheck = new Regex(@"^20[0-9]{2}$");
+            var cvvCheck = new Regex(@"^\d{3}$");
+
+            if (!cardCheck.IsMatch(cardNo)) // <1>check card number is valid
+                return false;
+            if (!cvvCheck.IsMatch(cvv)) // <2>check cvv is valid as "999"
+                return false;
+
+            var dateParts = expiryDate.Split('/'); //expiry date in from MM/yyyy            
+            if (!monthCheck.IsMatch(dateParts[0]) || !yearCheck.IsMatch(dateParts[1])) // <3 - 6>
+                return false; // ^ check date format is valid as "MM/yyyy"
+
+            var year = int.Parse(dateParts[1]);
+            var month = int.Parse(dateParts[0]);
+            var lastDateOfExpiryMonth = DateTime.DaysInMonth(year, month); //get actual expiry date
+            var cardExpiry = new DateTime(year, month, lastDateOfExpiryMonth, 23, 59, 59);
+
+            //check expiry greater than today & within next 6 years <7, 8>>
+            return (cardExpiry > DateTime.Now && cardExpiry < DateTime.Now.AddYears(6));
+        }*/
+        /// <summary>
+        /// THis function checks the date format to make sure it entered in the following formats: 
+        /// mm/yy || mm-yy.
+        /// </summary>
+        /// <param name="field"></param>
+        private static void IsDate(string field)
+        {
+            //var logger = scopedServices.GetRequiredService<ILogger<WebApplicationFactory<Startup>>>();
+            //logger.LogError(ex, "An error occured seeding the database with the message: " + ex.Message);
+            List<string> error = new List<string>();
+            Regex regex = new Regex(@"/^(0[1-9]|1[0-2])\/?([0-9]{2})$/");
+            Regex regex2 = new Regex(@"/^(0[1-9]|1[0-2])-/?([0-9]{2})$/");
             var dateMatch = field;
             Match match = regex.Match(dateMatch);
+            Match match2 = regex2.Match(dateMatch);
             {
-                if(!match.Success)
+                if (!match.Success || !match2.Success)
                 {
-                    throw new ArgumentException(nameof(field));
+                    error.Add("The expiration date must in the correct format(dd/yy or dd-yy)");
+                    //throw new ArgumentException(nameof(field);
                 }
             }
         }
         public void IsLength(string field, int minLength, int maxLength)
         {
-            //minLength = 0;
-           // maxLength = 0;
-                int length = field.Length;
-            if(length < minLength || length > maxLength)
+            List<string> error = null;
+            int length = field.Length;
+            if (length < minLength || length > maxLength)
             {
-                throw new ArgumentException(nameof(field));
+                error.Add("Must be more than 14 digits and less than 20 digits");
             }
         }
         public void IsNumber(string field)
         {
+            List<string> error = null;
             decimal cardNumber;
-            if(!Decimal.TryParse(field, out cardNumber))
+            if (!Decimal.TryParse(field, out cardNumber))
             {
-                throw new ArgumentException(nameof(field));
+                error.Add("Must be a number");
+                //throw new ArgumentException(nameof(field));
             }
         }
-        private void IsEmpty(string field)
+        public string IsEmpty(string field)
         {
-            if(field == null || field.Length == 0)
+            List<string> error = null;
+            if (field == null || field.Length == 0)
             {
-                throw new ArgumentException(nameof(field));
+                error.Add("Must not be left blank");
+                //throw new ArgumentException(nameof(field));
             }
+            return field;
         }
         public string CardNumberValidation(string number)
         {
-
+            int minLength = 14;
+            int maxLength = 19;
             var cardNumber = number.Trim();
-            
-
-            try
+            if (cardNumber.Length == 0)
             {
-                IsEmpty(cardNumber);
+                return IsEmpty(cardNumber);
             }
-            catch (Exception ex)
+            if (cardNumber.Length < minLength || cardNumber.Length > maxLength)
             {
-                return null;
-                //logger.LogError(ex, "An error occured seeding the database with the message: " + ex.Message);
-            }
-
-
-            try
-            {
-                IsNumber(cardNumber);
-            }
-            catch (Exception ex)
-            {
-
+                IsLength(cardNumber);
             }
             return cardNumber;
         }
 
-        public string[] CVVNumberValidation(string cvv)
+        public string CVVNumberValidation(string field)
         {
-            string cvvNumber = "";
-            int cvvLength = cvvNumber.Length;
-            if (cvvLength == 0 || cvvNumber == "")
+            int minLength = 3;
+            int maxLength = 4;
+            var cvvNumber = field.Trim();
+            if (cvvNumber.Length == 0)
             {
-                
+                IsEmpty(cvvNumber);
             }
-            return CVVNumberValidation(cvvNumber);
+            if (cvvNumber.Length > 0)
+            {
+                IsLength(field);
+                IsNumber(field);
+            }
+
+            return cvvNumber;
         }
-        
-        public string[] ExpirationDateValidation()
+
+        public string ExpirationDateValidation(string field)
         {
             string expirationDate = "";
             int lengthOfDate = expirationDate.Length;
-            DateTime today = DateTime.Now;
+
             if (expirationDate == "" || lengthOfDate == 0)
             {
-                throw new NotImplementedException("A valid Expiration date is required");
+                IsEmpty(expirationDate);
             }
-        
-            return ExpirationDateValidation();
-          
+
+            return field;
+
 
         }
-        
+
     }
 }
