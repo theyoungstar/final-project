fbb34040911e107992e34da36bae18b37b9c89aa
Leslie Mundell-Crucet <lmundell-crucet@catalyte.io>
2022-03-31 12:52:01 -0400

    add my http response to the solution file

---
+++ src/Catalyte.Apparel.Utilities/HttpResponseExceptions/UnprocessableEntityException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Catalyte.Apparel.Utilities.HttpResponseExceptions
+{
+    /// <summary>
+    /// A custom exception for resource not found errors.
+    /// </summary>
+    [Serializable]
+    public class UnprocessableEntity : Exception, IHttpResponseException
+    {
+        public UnprocessableEntity(string message)
+        {
+            Value = new(status: 422, error: "Purchase could not be completed the following products are not active {inactiveItemsList}.", message: message);
+        }
+        public HttpResponseExceptionValue Value { get; set; }
+    }
+}
