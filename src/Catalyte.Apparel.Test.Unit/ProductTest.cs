d4f6a9ca2e61b559893fd4280dbe1ab5b879acbb
Victoria Shelton <vshelton@catalyte.io>
2022-03-31 20:31:36 -0400

    add category tests

--- src/Catalyte.Apparel.Test.Unit/ProductTest.cs
+++ src/Catalyte.Apparel.Test.Unit/ProductTest.cs
@@ -13,25 +13,19 @@ namespace Catalyte.Apparel.Test.Unit.Product
     {
         private readonly ProductFactory _productFactory;
         List<string> _demographics = new List<string>();
+        List<string> _categories = new List<string>();
+        List<string> _types = new List<string>();
 
         public ProductTest()
         {
             _productFactory = new();
             _demographics = _productFactory.GetAllDemographics();
+            _categories = _productFactory.GetAllCategories();
+            _types = _productFactory.GetAllProductTypes();
         }
-
+      
         [Fact]
-        public void TestProductDemograpic_checkOneProduct()
-        {
-
-            List<Data.Model.Product> products = _productFactory.GenerateRandomProducts(1);
-            string demographic = products[0].Demographic;
-            Assert.Contains(demographic, _demographics);
-
-        }
-
-        [Fact]
-        public void TestProductDemograpic_checkManyProducts()
+        public void TestProductDemographic_checkManyProducts()
         {
 
             List<Data.Model.Product> products = _productFactory.GenerateRandomProducts(100);
@@ -44,19 +38,19 @@ namespace Catalyte.Apparel.Test.Unit.Product
         }
 
         [Fact]
-        public void TestProductDemograpic_checkForNullDemographics()
+        public void TestProductDemographic_checkForNullDemographics()
         {
             List<Data.Model.Product> products = _productFactory.GenerateRandomProducts(100);
             foreach (var product in products)
             {
-                string demographic = products[0].Demographic;
+                string demographic = product.Demographic;
                 Assert.NotNull(demographic);
             }
 
         }
 
         [Fact]
-        public void TestProductDemograpic_checkForCorrectNumberOfDemographics()
+        public void TestProductDemographic_checkForCorrectNumberOfDemographics()
         {
             List<Data.Model.Product> products = _productFactory.GenerateRandomProducts(100);
             Dictionary<string, int> demographicCount = new Dictionary<string, int>();
@@ -69,6 +63,88 @@ namespace Catalyte.Apparel.Test.Unit.Product
                 demographicCount[product.Demographic]++;
             }
             Assert.Equal(_demographics.Count, demographicCount.Count);
+        }
+
+        [Fact]
+        public void TestProductCategory_checkManyProducts()
+        {
+
+            List<Data.Model.Product> products = _productFactory.GenerateRandomProducts(100);
+            foreach (var product in products)
+            {
+                string category = product.Category;
+                Assert.Contains(category, _categories);
+            }
+
+        }
+
+        [Fact]
+        public void TestProductCategory_checkForNull()
+        {
+            List<Data.Model.Product> products = _productFactory.GenerateRandomProducts(100);
+            foreach (var product in products)
+            {
+                string category = product.Category;
+                Assert.NotNull(category);
+            }
+
+        }
+
+        [Fact]
+        public void TestProductCategory_checkForCorrectNumberOfCategories()
+        {
+            List<Data.Model.Product> products = _productFactory.GenerateRandomProducts(100);
+            Dictionary<string, int> categoryCount = new Dictionary<string, int>();
+            foreach (var product in products)
+            {
+                if (!(categoryCount.TryGetValue(product.Category, out int count)))
+                {
+                    categoryCount.Add(product.Category, 0);
+                }
+                categoryCount[product.Category]++;
+            }
+            Assert.Equal(_categories.Count, categoryCount.Count);
+        }
+
+        [Fact]
+        public void TestProductType_checkManyProducts()
+        {
+
+            List<Data.Model.Product> products = _productFactory.GenerateRandomProducts(100);
+            foreach (var product in products)
+            {
+                string type = product.Type;
+                Assert.Contains(type, _types);
+            }
+
+        }
+
+        [Fact]
+        public void TestProductType_checkForNull()
+        {
+            List<Data.Model.Product> products = _productFactory.GenerateRandomProducts(100);
+            foreach (var product in products)
+            {
+                string type = product.Type;
+                Assert.NotNull(type);
+            }
+
+        }
+
+        [Fact]
+        public void TestProductType_checkForCorrectNumberOfTypes()
+        {
+            List<Data.Model.Product> products = _productFactory.GenerateRandomProducts(100);
+            Dictionary<string, int> typeCount = new Dictionary<string, int>();
+            foreach (var product in products)
+            {
+                if (!(typeCount.TryGetValue(product.Type, out int count)))
+                {
+                    typeCount.Add(product.Type, 0);
+                }
+                typeCount[product.Type]++;
+            }
+            Assert.Equal(_types.Count, typeCount.Count);
         }
     }
 }
