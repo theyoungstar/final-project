497edcddee442b291d1072b8c6008c687349fbeb
Edward Waters <ewaters@catalyte.io>
2022-04-01 18:25:45 -0400

    split color filter to 2 methods, for Primary and Secondary colors

--- src/Catalyte.Apparel.Data/Filters/ProductFilters.cs
+++ src/Catalyte.Apparel.Data/Filters/ProductFilters.cs
@@ -33,9 +33,13 @@ namespace Catalyte.Apparel.Data.Filters
         {
             return products.Where(p => p.Price == price).AsQueryable();
         }
-        public static IQueryable<Product> WhereProductColorEquals(this IQueryable<Product> products, string color)
+        public static IQueryable<Product> WhereProductPrimaryColorEquals(this IQueryable<Product> products, string color)
         {
-            return products.Where(p => p.Color == color).AsQueryable();
+            return products.Where(p => p.PrimaryColorCode == color).AsQueryable();
+        }
+        public static IQueryable<Product> WhereProductSecondaryColorEquals(this IQueryable<Product> products, string color)
+        {
+            return products.Where(p => p.SecondaryColorCode == color).AsQueryable();
         }
         public static IQueryable<Product> WhereProductMaterialEquals(this IQueryable<Product> products, string material)
         {
