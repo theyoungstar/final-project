cd89fbe17bd715bfccf329a1cf6787124f90ef37
kmedeiros <kmedeiros@catalyte.io>
2022-04-29 18:03:41 -0500

    fixing branch conflicts

--- src/Catalyte.Apparel.Test.Integration/PromoCodeIntegrationTests.cs
+++ src/Catalyte.Apparel.Test.Integration/PromoCodeIntegrationTests.cs
@@ -94,5 +94,46 @@ namespace Catalyte.Apparel.Test.Integration
                 _promoCodeRepository.DeletePromoCodesAsync(actual);
             }
         }
+
+        [Fact]
+        public async Task GetPromoCodesAsync_GivenRateLessOrEqualTo100_ReturnsPromoCodes()
+        {
+            var promoCodeDTO = new CreatePromoCodeDTO()
+            {
+                Id = 7,
+                Title = "CODE07",
+                Type = "%",
+                Description = "Description of Promo",
+                Rate = 50,
+            };
+
+            var postPromoCode = JsonContent.Create(promoCodeDTO);
+            var post = await _client.PostAsync("/promocodes", postPromoCode);
+            Assert.Equal(HttpStatusCode.Created, post.StatusCode);
+
+            var response = await _client.GetAsync("/promocodes");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var content = await response.Content.ReadAsAsync<IEnumerable<PromoCodeDTO>>();
+            Assert.Equal(7, content.FirstOrDefault().Id);
+        }
+
+        [Fact]
+        public async Task GetPromoCodesAsync_GivenRateOver100_ThrowsBadRequestException()
+        {
+            var promoCodeDTO = new CreatePromoCodeDTO()
+            {
+                Id = 8,
+                Title = "CODE08",
+                Type = "%",
+                Description = "Description of Promo",
+                Rate = 123,
+            };
+
+            var postPromoCode = JsonContent.Create(promoCodeDTO);
+            var post = await _client.PostAsync("/promocodes", postPromoCode);
+            Assert.Equal(HttpStatusCode.BadRequest, post.StatusCode);
+        }
+
     }
 }
