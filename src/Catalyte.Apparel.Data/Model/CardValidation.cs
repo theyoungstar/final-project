407bb93c91032667ecf034560c6c3dcf07b4cb44
Julius Amaker <jamaker@catalyte.io>
2022-05-21 22:05:51 -0700

    Fixed Card Validation. It now throws an error for null values

--- src/Catalyte.Apparel.Data/Model/CardValidation.cs
+++ src/Catalyte.Apparel.Data/Model/CardValidation.cs
@@ -9,17 +9,17 @@ namespace Catalyte.Apparel.Data.Model
 
     public class CardValidation
     {
-    /// <summary>
-    /// This is the Credit Card validation. It validates that the card fields meet required specifications before
-    /// the purchase goes to the Database
-    /// </summary>
-    /// <param name="purchase"></param>
-    /// <returns>errorsList</returns>
+        /// <summary>
+        /// This is the Credit Card validation. It validates that the card fields meet required specifications before
+        /// the purchase goes to the Database
+        /// </summary>
+        /// <param name="purchase"></param>
+        /// <returns>errorsList</returns>
         public virtual List<string> CreditCardValidation(Purchase purchase)
         {
             List<string> errorsList = new List<string>();
             Has14To19Digits(purchase.CardNumber, errorsList);
-            CVVHas3To4Digits(purchase.CVV, errorsList);          
+            CVVHas3To4Digits(purchase.CVV, errorsList);
             IsValidExpirationDate(purchase.Expiration, errorsList);
             CardHolderName(purchase.CardHolder, errorsList);
 
@@ -31,19 +31,20 @@ namespace Catalyte.Apparel.Data.Model
         /// <param name="field"></param>
         /// <param name="errorsList"></param>
         /// <returns>the boolean true</returns>
-        public bool Has14To19Digits(string field, List<string> errorsList)
-        {   
-            if(field.Length < 1)
+        public void Has14To19Digits(string field, List<string> errorsList)
+        {
+            if ((field == null) || field.Length < 1)
             {
-                errorsList.Add("Card number is required");
+                errorsList.Add("Card number is required ");
             }
-            var cardCheck = new Regex(@"^[0-9]{14,19}$");
-            if (!cardCheck.IsMatch(field.Trim()) && field.Length > 0) 
+            else if (field.Length > 0)
             {
-                errorsList.Add("Card number must contain between 14 to 19 digits.");
-                return false;
+                var cardCheck = new Regex(@"^[0-9]{14,19}$");
+                if (!cardCheck.IsMatch(field.Trim()))
+                {
+                    errorsList.Add("Card number must contain between 14 to 19 digits ");
+                }
             }
-            return true;
         }
         /// <summary>
         /// This method verifies that the CVV has 3-4 digits, no letters, or special characters.
@@ -51,21 +52,20 @@ namespace Catalyte.Apparel.Data.Model
         /// <param name="field"></param>
         /// <param name="errorsList"></param>
         /// <returns>boolean</returns>
-        public bool CVVHas3To4Digits(string field, List<string> errorsList)
+        public void CVVHas3To4Digits(string field, List<string> errorsList)
         {
-            if(field.Length < 1)
+            if ((field == null) || field.Length < 1)
             {
-                errorsList.Add(" CVV is required");
+                errorsList.Add(" CVV is required ");
             }
-
-            var cvvCheck = new Regex(@"^[0-9]{3,4}$");
-            if (!cvvCheck.IsMatch(field) && field.Length > 0)
-              //checks if CVV field is empty or has between 3-4 digits  
+            else if (field.Length > 0)
             {
-                errorsList.Add(" CVV must have 3 or 4 digits.");
-                return false;
+                var cvvCheck = new Regex(@"^[0-9]{3,4}$");
+                if (!cvvCheck.IsMatch(field.Trim()))
+                {
+                    errorsList.Add(" CVV must have 3 or 4 digits ");
+                }
             }
-            return true;
         }
         /// <summary>
         /// This method verifies that the Expiration date is not empty, takes only numbers, a slash, or a dash, is in the
@@ -74,57 +74,52 @@ namespace Catalyte.Apparel.Data.Model
         /// <param name="field"></param>
         /// <param name="errorsList"></param>
         /// <returns>boolean</returns>
-        public bool IsValidExpirationDate(string field, List<string> errorsList)
+        public void IsValidExpirationDate(string field, List<string> errorsList)
         {
-            if (field == "")
+            if ((field == null) || field == "")
             {
-                errorsList.Add(" Expiration date is required");
+                errorsList.Add(" Expiration date is required ");
             }
-
-            Regex dateFormat = new Regex(@"^(0[1-9]|1[0-2])([\/-]{1})[0-9]{2}$");
-            //^ checks if date is in either of the following formats: mm/yy mm-yy
-            if (!dateFormat.IsMatch(field) && field.Length > 0)
+            else if (field.Length > 0)
             {
-                errorsList.Add(" Expiration date must have a month between 1-12, " +
-                    "contain no letters or special characters, and be entered in one of " +
-                    "the following formats: mm/yy or mm-yy");
-                return false;
+                Regex dateFormat = new Regex(@"^(0[1-9]|1[0-2])([\/-]{1})[0-9]{2}$");
+                if (!dateFormat.IsMatch(field))
+                {
+                    errorsList.Add("Expiration date must have a month between 1-12, " +
+                "contain no letters or special characters, and be entered in one of " +
+                "the following formats: mm/yy or mm-yy ");
+                }
+                else if (dateFormat.IsMatch(field.Trim()))
+                {
+                    string[] dateParts = new string[0];
+                    if (field.Trim().Contains('/'))
+                    {
+                        dateParts = field.Split('/');
+                    }
+                    else if (field.Trim().Contains('-'))
+                    {
+                        dateParts = field.Split('-');
+                        var month = int.Parse(dateParts[0]);
+                        var shortYear = int.Parse(dateParts[1]);
+                        var stringYear = $"20{shortYear}";
+                        var year = int.Parse(stringYear);
+
+                        var lastDateOfExpirationMonth = DateTime.DaysInMonth(year, month);
+                        var cardExpiration = new DateTime(year, month, lastDateOfExpirationMonth, 23, 59, 59);
+
+                    
+                        if (cardExpiration < DateTime.Now)
+                        {
+                            errorsList.Add(" Expiration date must be a future date");
+
+                        }
+                    }
+
+                }
+
             }
 
-            if (dateFormat.IsMatch(field.Trim()) && field.Length > 0)
-            { //expiration date in from mm/yy
-                string[] dateParts = new string[0];
-                //Array to catch date parts
 
-                if (field.Trim().Contains('/'))
-                {
-                    dateParts = field?.Split('/');
-                }
-
-                if (field.Trim().Contains('-'))
-                {
-                    dateParts = field?.Split('-');
-                }
-
-                var month = int.Parse(dateParts?[0]);
-                var shortYear = int.Parse(dateParts?[1]);
-                //converts date parts to int
-                var stringYear = $"20{shortYear}";
-                //interpolates short year to 4 digit year
-                var year = int.Parse(stringYear);
-                //converts year string to int
-
-                var lastDateOfExpirationMonth = DateTime.DaysInMonth(year, month); //gets actual expiration date
-                var cardExpiration = new DateTime(year, month, lastDateOfExpirationMonth, 23, 59, 59);
-
-                //check if expiration date less than today
-                if (cardExpiration < DateTime.Now)
-                {
-                    errorsList.Add(" Expiration date must be a future date");
-
-                }
-            }
-                return true;
         }
         /// <summary>
         /// This method verifies that the Cardholder name doesn't contain numbers and takes only letters, spaces, 
@@ -133,23 +128,22 @@ namespace Catalyte.Apparel.Data.Model
         /// <param name="field"></param>
         /// <param name="errorsList"></param>
         /// <returns>boolean</returns>
-        public bool CardHolderName(string field, List<string> errorsList)
+        private static void CardHolderName(string field, List<string> errorsList)
         {
-            if(field.Length < 1)
+            if ((field == null) || field.Length < 1)
             {
                 errorsList.Add(" Card Holder name required");
             }
-            Regex cardHolder = new Regex(@"^[a-zA-Z-' ]+[ ]{1}[a-zA-Z-' ]{2,}([^0-9]*)$");
-            //checks if field has only letters and spaces
+                else if(field.Length > 1)
+                {
+                Regex cardHolder = new Regex(@"^[a-zA-Z-' ]+[ ]{1}[a-zA-Z-' ]{2,}([^0-9]*)$");
+                    if (!cardHolder.IsMatch(field.TrimEnd().TrimStart()))
+                    {
+                    errorsList.Add(" Cardholder name can only contain spaces, apostrohes, and letters");
+                    }
 
-            if (!cardHolder.IsMatch(field.TrimEnd().TrimStart()) && field.Length > 0 )
-            {
-                errorsList.Add(" Cardholder name must contain letters only");
-                return false;
-            }
-            return true;
+                }
         }
-
 
     }
 }
